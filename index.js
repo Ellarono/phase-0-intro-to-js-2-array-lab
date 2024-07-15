e9c00c8d21a28ed10bc57de5e9b2f33847496f5d
Daniella Rono <daniellarono@gmail.com>
2024-07-15 04:34:30 +0300

    all tests are passing

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield']
+function destructivelyAppendCat(name){
+    return cats.push(name)
+}
+function destructivelyPrependCat(name){
+    return cats.unshift(name)
+
+}
+function destructivelyRemoveLastCat(name){
+    return cats.pop(name)
+}
+function destructivelyRemoveFirstCat(name){
+
+    return cats.shift(name)
+}
+function appendCat(name){
+    const newCats =[...cats,name]
+    return newCats
+}
+function prependCat(name){
+    const newCats =[name,...cats];
+    return newCats
+}
+function removeLastCat (name){
+return cats.slice(0,-1)
+}
+function removeFirstCat (name){
+    return cats.slice(1)
+
+}
